f9142f6794b10158abdab4b37e70e7e46bc1d627
ammyarya <amitarya13072000@gmail.com>
2026-04-26 12:25:05 +0530

    Fix Swagger UI backend API server URLs resolving to internal docker hostname

--- worker-service/src/main/java/com/example/worker/WorkerServiceApplication.java
+++ worker-service/src/main/java/com/example/worker/WorkerServiceApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 public class WorkerServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(WorkerServiceApplication.class, args);
